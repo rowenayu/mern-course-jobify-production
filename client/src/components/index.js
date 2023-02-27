c8dd1abd072d93101c88af86a6ef8551fdacd575
rowenayu <rowenayu96@gmail.com>
2023-02-27 15:42:53 +1100

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+import Alert from './Alert';
+import BigSidebar from './BigSidebar';
+import ChartsContainer from './ChartsContainer';
+import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
+import JobsContainer from './JobsContainer';
+import Loading from './Loading';
+import Logo from './Logo';
+import NavBar from './NavBar';
+import SearchContainer from './SearchContainer';
+import SmallSidebar from './SmallSidebar';
+import StatsContainer from './StatsContainer';
+
+export { Logo, FormRow, Alert, NavBar, BigSidebar, SmallSidebar,FormRowSelect, SearchContainer, JobsContainer,StatsContainer, ChartsContainer, Loading }
---
+++ client/src/pages/dashboard/Stats.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { ChartsContainer, StatsContainer, Loading } from "../../components";
+import { useAppContext } from "../../context/appContext";
+
+const Stats = () => {
+  const { showStats, isLoading, monthlyApplications } = useAppContext();
+
+  useEffect(() => {
+    showStats();
+    // eslint-disable-next-line
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
+};
+
+export default Stats;
